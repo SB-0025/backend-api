1aa1cfd8f12dbbfe919d23b9768f44b644c70363
Sambhab Bose <bosesambhab2020@gmail.com>
2026-03-29 01:01:45 +0530

    configure middlewares and  add custom api error and response utility classes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-import "dotenv/config"
-import connectDB from "./db/dbConnect.js"
+import "dotenv/config";
+import connectDB from "./db/dbConnect.js";
+import app from "./app.js";
 
-connectDB()
+const port = process.env.PORT || 3000;
 
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.on("error", (err) => {
+      console.log("Server Error : ", err);
+    });
+    app.listen(port, () => console.log(`Server is running on port ${port}`));
+  } catch (error) {
+    console.error("DB CONNECTION FAILED:", error.message)
+    process.exit(1)
+  }
+};
+
+startServer()
