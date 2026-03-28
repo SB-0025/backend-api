3631f6c8b78971641e73042d852e3d628682acaf
Sambhab Bose <bosesambhab2020@gmail.com>
2026-03-29 01:11:13 +0530

    fix bugs

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,10 @@
 class ApiResponse {
     constructor(statusCode, data, message = "Success"){
-        this.statusCode = statusCode <400 
+        this.statusCode = statusCode  
         this.data = data
         this.success = true
         this.message = message
     }
 }
+
+export default ApiResponse
